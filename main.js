b0e9954e88b479dee84e96b9f995c33619300a70
Ecnamore <vlad.atamanivol@gmail.com>
2023-08-09 12:08:27 +0300

    Project files added

---
+++ main.js
@@ -0,0 +1,44 @@
+import express, { response } from 'express'
+import config from 'config'
+import { engine } from 'express-handlebars'
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+    apiKey: "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"
+})
+const openai = new OpenAIApi(configuration)
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_, res) => {
+    res.render('index')
+})
+
+app.post('/', async (req, res) => {
+    const prompt = req.body.prompt
+    const size = req.body.size ?? '512x512'
+    const number = req.body.number ?? 1
+
+    try {
+       const response = await openai.createImage({
+            createImageRequest: CreateImageRequest
+        })
+
+        console.log(response)
+
+        // Передача данных response в шаблон для отображения на клиенте
+        res.render('index')   
+    } catch (e) {
+        // Передача сообщения об ошибке в шаблон для отображения на клиенте
+        res.render('index', {
+           error: e.message,
+        })
+    }
+})
+
+app.listen(3000, () => console.log('Server start...'))

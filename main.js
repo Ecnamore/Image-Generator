f617e806d9329514e2959ba38c9dbbad948bd03c
Ecnamore <vlad.atamanivol@gmail.com>
2023-08-10 12:16:47 +0300

    formating fix

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-import express, { response } from 'express'
-import config from 'config'
-import { engine } from 'express-handlebars'
-import { Configuration, OpenAIApi } from 'openai'
+import express, { response } from "express"
+import config from "config"
+import { engine } from "express-handlebars"
+import { Configuration, OpenAIApi } from "openai"
 
 const configuration = new Configuration({
-    apiKey: config.get ('OPENAI_KEY'),
+  apiKey: config.get("OPENAI_KEY"),
 })
 const openai = new OpenAIApi(configuration)
 
 const app = express()
 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', './views')
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", "./views")
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (_, res) => {
-    res.render('index')
+app.get("/", (_, res) => {
+  res.render("index")
 })
 
-app.post('/', async (req, res) => {
-    const prompt = req.body.prompt
-    const size = req.body.size ?? '512x512'
-    const number = req.body.number ?? 1
+app.post("/", async (req, res) => {
+  const prompt = req.body.prompt
+  const size = req.body.size ?? "512x512"
+  const number = req.body.number ?? 1
 
-    try {
-        const response = await openai.createImage({
-            prompt,
-            n: Number (number),
-            size,
-          });
-        
-         console.log(response)
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: Number(number),
+      size,
+    })
 
-        // Передача данных response в шаблон для отображения на клиенте
-        res.render('index')   
-    } catch (e) {
-        // Передача сообщения об ошибке в шаблон для отображения на клиенте
-        res.render('index', {
-           error: e.message,
-        })
-    }
+    console.log(response)
+
+    // Передача данных response в шаблон для отображения на клиенте
+    res.render("index")
+  } catch (e) {
+    // Передача сообщения об ошибке в шаблон для отображения на клиенте
+    res.render("index", {
+      error: e.message,
+    })
+  }
 })
 
-app.listen(3000, () => console.log('Server start...'))
+app.listen(3000, () => console.log("Server start..."))
